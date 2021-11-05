1e12c7abc25b0cd2498efb29426ef36d81cf1afe
Josh Cornau <hi@joshcornau.de>
2021-11-05 20:25:09 +0100

    scrapyyyy

--- LeicaScraping.py
+++ LeicaScraping.py
@@ -21,12 +21,48 @@ def getnextpage(soup):
         return
 
 
+
+
+def crawlcomments(soup):
+    for article in soup.find_all('article', id=lambda x: x and x.startswith('elComment_')):
+        autor = article.find('aside', class_='ipsComment_author').h3.text
+        print (autor)
+        try: 
+            posts = article.find('aside', class_='ipsComment_author').ul.find('li', attrs={'data-role' : 'posts'}).text
+        except Exception as e:
+            posts = None
+        print (posts)
+        try: 
+            date = article.find('div', class_='ipsColumn').find('time')['title']
+        except Exception as e:
+            date = None
+        if date!=None:
+            print (date)
+        try: 
+            content = article.find('div', attrs={'data-role' : 'commentContent'}).text
+        except Exception as e:
+            content = None
+        print (content)
+
+        csv_writer.writerow([autor,posts,date,content])
+
+
+csv_file = open('leicaScrape2.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Autor','Posts','Date','content'])
+
+
+
+
+
 while True:
     soup = getdata(url)
-    crawlcomments
+    crawlcomments(soup)
     url = getnextpage(soup)
     if not url:
+        csv_file.close()
         break
+
     print(url)
 
 
@@ -38,36 +74,10 @@ while True:
 # soup = BeautifulSoup(source, 'lxml')
 
 
-# csv_file = open('leicaScrape2.csv', 'w')
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['Autor','Posts','Date','content'])
 
 
 
-# for article in soup.find_all('article', id=lambda x: x and x.startswith('elComment_')):
-#     autor = article.find('aside', class_='ipsComment_author').h3.text
-#     print (autor)
-#     try: 
-#         posts = article.find('aside', class_='ipsComment_author').ul.find('li', attrs={'data-role' : 'posts'}).text
-#     except Exception as e:
-#         posts = None
-#     print (posts)
-#     try: 
-#         date = article.find('div', class_='ipsColumn').find('time')['title']
-#     except Exception as e:
-#         date = None
-#     if date!=None:
-#         print (date)
-#     try: 
-#         content = article.find('div', attrs={'data-role' : 'commentContent'}).text
-#     except Exception as e:
-#         content = None
-#     print (content)
-
-#     csv_writer.writerow([autor,posts,date,content])
-
-
-# csv_file.close()
+# 
 
 
 
